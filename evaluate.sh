15dd841347c66fe4457a0656fa86917114d2d444
Vít Novotný <witiko@mail.muni.cz>
2020-03-12 22:05:00 +0100

    Update result tables

--- evaluate.sh
+++ evaluate.sh
@@ -3,6 +3,7 @@ set -e
 shopt -s nullglob
 
 cd task1
+# summary task 1 table header
 cat > README-head.md << EOT
 | User | nDCG | Result name |
 |:-----|------|:------------|
@@ -10,6 +11,7 @@ EOT
 for USER in */
 do
   cd $USER
+  # per-user task 1 table header
   cat > README-head.md << EOT
 | nDCG | Result name |
 |------|:------------|
@@ -17,19 +19,23 @@ EOT
   for RESULT in *.tsv
   do
     NDCG=$(../../trec_eval/trec_eval ../qrel.V0.1.tsv "$RESULT" -m ndcg | awk '{ print $3 }')
-    cat >> ../README-tail.md << EOT
-| [${USER%/}](https://gitlab.fi.muni.cz/$USER) | $NDCG | $(printf '%s\n' "${RESULT%.tsv}" | sed 's/_/, /g') |
-EOT
+    # per-user task 1 table entries
     cat >> README-tail.md << EOT
 | $NDCG | $(printf '%s\n' "${RESULT%.tsv}" | sed 's/_/, /g') |
 EOT
   done
-  (cat README-head.md && sort -k 2 -k 4 README-tail.md) > README.md
+  (cat README-head.md && LC_ALL=C sort -k 2 -k 4 README-tail.md | tee >(
+    # summary task 1 table header
+    head -1 | while read LINE
+    do
+      printf '%s%s\n' "| [${USER%/}](https://gitlab.fi.muni.cz/$USER) " "$LINE"
+    done >> ../README-tail.md
+  )) > README.md
   rm README-head.md README-tail.md
   git add README.md
   cd ..
 done
-(cat README-head.md && sort -k 4 -k 2 -k 6 README-tail.md) > README.md
+(cat README-head.md && LC_ALL=C sort -k 4 -k 2 -k 6 README-tail.md) > README.md
 rm README-head.md README-tail.md
 git add README.md
 cd ..
