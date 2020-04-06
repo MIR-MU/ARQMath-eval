e69e56f8f97009ff3ce42b1562266beb9b7fa416
Vít Novotný <witiko@gmail.com>
2020-04-06 12:41:29 +0200

    Create scripts.common.remove_nonjudged_topics_and_documents

--- scripts/common.py
+++ scripts/common.py
@@ -7,7 +7,7 @@ import numpy as np
 from .configuration import EVALUATORS, PARSED_RELEVANCE_JUDGEMENTS
 
 
-def ndcg(parsed_run, task='task1', subset='train'):
+def remove_nonjudged_topics_and_documents(parsed_run, task, subset):
     only_judged_parsed_run = deepcopy(parsed_run)
     parsed_relevance_judgements = PARSED_RELEVANCE_JUDGEMENTS[subset][task]
     for topic_name, results in parsed_run.items():
@@ -18,7 +18,12 @@ def ndcg(parsed_run, task='task1', subset='train'):
             for document_name in results.keys():
                 if document_name not in judgements:
                     del only_judged_parsed_run[topic_name][document_name]
+    return only_judged_parsed_run
+
+
+def ndcg(parsed_run, task='task1', subset='train'):
     evaluator = EVALUATORS[subset][task]
+    only_judged_parsed_run = remove_nonjudged_topics_and_documents(parsed_run, task, subset)
     evaluation = evaluator.evaluate(only_judged_parsed_run)
     ndcg = np.mean([
         measures['ndcg']
