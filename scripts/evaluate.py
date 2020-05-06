2afb071eb2fa9fd1e4e415d0147a4b552009db74
Vít Novotný <witiko@gmail.com>
2020-05-06 17:36:56 +0200

    Add random NDCG' to the leaderboards

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -6,12 +6,13 @@ import re
 
 from pytrec_eval import parse_run
 
-from .common import get_ndcg
+from .common import get_ndcg, get_random_ndcg
 from .configuration import TASKS, USER_README_HEAD
 
 
 if __name__ == '__main__':
     for task in TASKS:
+        random_ndcg = get_random_ndcg(task, 'validation')
         for user in glob(os.path.join(task, '*', '')):
             user = os.path.normpath(user)
             user_name = os.path.basename(user)
@@ -20,9 +21,10 @@ if __name__ == '__main__':
                 result_name = re.sub('_', ', ', os.path.basename(result)[:-4])
                 with open(result, 'rt') as f:
                     parsed_result = parse_run(f)
-                user_results.append((get_ndcg(parsed_result, task, 'validation'), result_name))
+                ndcg = get_ndcg(parsed_result, task, 'validation')
+                user_results.append((ndcg, result_name))
             with open(os.path.join(user, 'README.md'), 'wt') as f:
-                f.write(USER_README_HEAD % user_name)
+                f.write(USER_README_HEAD % (user_name, random_ndcg))
                 f.write('\n')
                 for ndcg, result_name in sorted(user_results, reverse=True):
                     f.write('| %.4f | %s |\n' % (ndcg, result_name))
--- task1-votes/xnovot32/README.md
+++ task1-votes/xnovot32/README.md
@@ -1,6 +1,7 @@
 This table contains all results for user *xnovot32* in descending order of task
 performance.  Result names are based on the filenames of the results with
 underscores (`_`) replaced with a comma and a space for improved readability.
+The random nDCG for this task is 0.0115.
 
 | nDCG | Result name |
 |------|:------------|
